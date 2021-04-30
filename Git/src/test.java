c42eb6a966d657a3d92fb20ff0e71783f4652bc6
maxbr <maxbr@MSI-Max.fritz.box>
2021-04-30 11:15:11 +0200

    huhu

--- Git/src/test.java
+++ Git/src/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// von Max
-		System.out.println("DIES IST EIN TEST DU MUGGL");
+		System.out.println("TEST DU MUGGL");
 	}
 
 }
