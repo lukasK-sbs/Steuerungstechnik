6f592fbad8e83db3343ee7ddad537c9583cb26bf
johan <johan@DESKTOP-855CTHH.lan>
2021-04-30 12:06:04 +0200

    fg

--- Git/src/test.java
+++ Git/src/test.java
@@ -2,14 +2,9 @@
 public class test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("tesfgfdfgfgfgfdgt");
 
-=======
-		// TODO Auto-generated method stub
-		// von Max
-		System.out.println("TEST DU MUGGLBERTI");
->>>>>>> branch 'main' of https://github.com/lukasK-sbs/Steuerungstechnik
 	}
 
 }
