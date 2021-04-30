26532beb88fb9772e3acef53cb9263ba9ac8778a
B-Mex <79988232+B-Mex@users.noreply.github.com>
2021-04-30 11:52:48 +0200

    haha

--- Git/src/test.java
+++ Git/src/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// von Max
-		System.out.println("TEST DU MUGGL");
+		System.out.println("TEST DU MUGGLBERTI");
 	}
 
 }
