f9c8eb303f77f3c00faa374d2c82b12ac950c6d8
fsmt1_12 <fsmt1_12@EG19_12.domino1.local>
2021-05-14 12:20:50 +0200

    commit

--- Git/src/test.java
+++ Git/src/test.java
@@ -2,7 +2,8 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("fgfgfdfdfgsdfasdfasdf");
+		System.out.println("auch ich");
+		System.out.println("Hallo");
 		System.out.println("Hertz");
 
 	}
