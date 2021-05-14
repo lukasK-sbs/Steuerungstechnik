e87524dcd423309a0684730d3b38d97f28360e7b
BrainBug <BrainBug@DESKTOP-N7V1IOU>
2021-05-14 12:17:27 +0200

    Nochmal hertz

--- Git/src/test.java
+++ Git/src/test.java
@@ -2,7 +2,7 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("fgfgfdfdfgsdfasdfasdf");
+		System.out.println("auch ich");
 		System.out.println("Hertz");
 
 	}
