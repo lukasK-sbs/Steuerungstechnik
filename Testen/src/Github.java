843fbd2da405a53c2eb570056532fe5c70d65ba5
fsmt1_5 <fsmt1_5@UG101_05.domino1.local>
2021-05-17 11:57:02 +0200

    asdf

--- Testen/src/Github.java
+++ Testen/src/Github.java
@@ -4,7 +4,7 @@ public class Github {
 	public static void main(String[] args) {
 		System.out.println("test");
 System.out.println("hallo");
-System.out.println("hallasdfasdasdfo");
+System.out.println("hallasdfasasdfdasdfo");
 	}
 
 }
