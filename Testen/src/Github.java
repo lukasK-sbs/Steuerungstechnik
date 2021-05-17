dc9f4c46721c1c299c33fe4eb1a04c946050956d
FSMT1_10 <FSMT1_10@UG101_06.domino1.local>
2021-05-17 12:09:07 +0200

    felix

--- Testen/src/Github.java
+++ Testen/src/Github.java
@@ -2,13 +2,10 @@
 public class Github {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-<<<<<<< HEAD
-		System.out.println("tschüss");
-=======
-System.out.println("hallo");
-System.out.println("lukas");
->>>>>>> branch 'dev' of https://github.com/lukasK-sbs/Steuerungstechnik
+		System.out.println("hallo");
+		System.out.println("lukas");
+		System.out.println("felix");
+		
 	}
 
 }
